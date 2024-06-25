ec8fd64a0ede432b638e1e31198e26ce0f916195
Mrunmayi28 <mrpatankar28@gmail.com>
2024-06-25 10:03:50 +0530

    Remove Imported statement from App.js file

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Counter from './Counter'; // Assuming Counter component is still in use
-import TodoList from './TodoList';
+// import TodoList from './TodoList';
 
 const App = () => {
   return (
